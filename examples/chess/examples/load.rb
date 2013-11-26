ef9939bbfac38b3836bafa0bfe2bcfa0a80c542e
Bernard Lambeau <blambeau@gmail.com>
2013-11-26 09:19:52 +0100

    Update the example.

--- examples/chess/examples/load.rb
+++ examples/chess/examples/load.rb
@@ -2,4 +2,14 @@ require_relative 'env'
 
 data = (Path.dir/'board.json').load
 board = Chess::Board.alpha(data)
-puts board.to_yaml
+
+puts board
+
+# puts Chess::Board.omega(board)
+#                  .down(:piece)
+#                  .unwrap(:piece)
+#                  .summarize([:color], count: count())
+
+# puts Relation.alpha(data)
+#              .unwrap(:piece)
+#              .summarize([:color], count: count())
