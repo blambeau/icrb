7c790d84686f2d12be55a8ba709f531a99e7db94
Bernard Lambeau <blambeau@gmail.com>
2013-11-27 18:19:50 +0100

    Spec for unnamed contracts

---
+++ spec/acceptance/test_unnamed_contract.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+module ICRb
+  describe "Using an unnamed contract" do
+
+    class WithUnnamedContract
+      include ICRb::ADT
+
+      def initialize(rep)
+        @rep = rep
+      end
+      attr_reader :rep
+
+      ic String do
+
+        def valid?(s)
+          s =~ /^[0-9]+$/
+        end
+
+        def dress(s)
+          WithUnnamedContract.new(s)
+        end
+
+        def undress(wim)
+          wim.to_s
+        end
+
+      end # ic String
+
+    end # class WithUnnamedContract
+
+    it 'should still work' do
+      WithUnnamedContract.dress('123').rep.should eq("123")
+    end
+
+    it 'should not install a .default class method' do
+      WithUnnamedContract.should_not respond_to(:default)
+    end
+
+    it 'should not install a to_default instance method' do
+      WithUnnamedContract.new('123').should_not respond_to(:to_default)
+    end
+  end
+end
